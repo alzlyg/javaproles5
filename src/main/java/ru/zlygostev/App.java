08e1e4dcd80caee79d71d51bfe2c71d86950c021
Alexey <webmaster.innobi@gmail.com>
2018-11-01 19:05:31 +0300

    Урок 5. Многопоточность

---
+++ src/main/java/ru/zlygostev/App.java
@@ -0,0 +1,13 @@
+package ru.zlygostev;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
